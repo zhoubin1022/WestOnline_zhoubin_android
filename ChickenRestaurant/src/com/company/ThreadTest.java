b9a1c4c5e100b5d0ce31ad5001c0bc617d220f81
zbgg <62539982+zhoubin1022@users.noreply.github.com>
2020-12-12 00:43:06 +0800

    Update ThreadTest.java
    更新多线程代码

--- ChickenRestaurant/src/com/company/ThreadTest.java
+++ ChickenRestaurant/src/com/company/ThreadTest.java
@@ -1,39 +1,48 @@
 import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 /**
  * create By  on 2020/11/9.
  */
-public class ThreadTest extends Thread {
-    private int index;
-    private int x;
+public class ThreadTest {
+    static int x;
     static long ans=0;
-    public ThreadTest(int index,int x) {
-        this.index = index;this.x=x;
-    }
 
-    public static void main(String[] args) {
-        long startTime=System.currentTimeMillis();
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
+        x = scanner.nextInt();
+        long startTime=System.currentTimeMillis();
         for (int i = 0; i < 10; i++) {
-            Thread t= new ThreadTest(i,x);
-            t.start();
-        }
-        while(Thread.activeCount()>1){
-            Thread.yield();
+            TaskThread t= new TaskThread(i*100000000,(i+1)*100000000);
+            FutureTask futureTask=new FutureTask(t);
+            new Thread(futureTask).start();
+            ans+=(long)futureTask.get();
         }
         System.out.println(ans);
         long endTime=System.currentTimeMillis();
         System.out.println("Time:"+(endTime-startTime));
     }
-    public synchronized void run(){//100000000
-        for(long  i=index*100000000;i<(index+1)*100000000;i++){
-            if(contain(i,x)){
-                ans+=i;
-            }
-        }
-    }
     private static boolean contain(long num, int x) {
         return String.valueOf(num).contains(String.valueOf(x));
     }
+    static class TaskThread implements Callable {
+        private long s, e;
+        public TaskThread(long s, long e) {
+            this.s = s;
+            this.e = e;
+        }
+        @Override
+        public Object call(){
+            long result = 0;
+            for (long i = s; i <e; i++) {
+                if (contain(i,x)) {
+                    result += i;
+                }
+            }
+            return result;
+        }
+    }
+
 }
